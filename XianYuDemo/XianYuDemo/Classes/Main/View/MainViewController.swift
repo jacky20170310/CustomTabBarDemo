80aa9e3e327a75b628d53f32e5605715c9771dc1
enrica <aishiqichun@163.com>
2017-10-14 22:55:01 +0800

    添加子控制器

--- XianYuDemo/XianYuDemo/Classes/Main/View/MainViewController.swift
+++ XianYuDemo/XianYuDemo/Classes/Main/View/MainViewController.swift
@@ -13,23 +13,62 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 统一设置UI界面
+        setupUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+
+// MARK: - 设置UI界面
+extension MainViewController {
+    
+    /// 统一设置UI界面
+    fileprivate func setupUI() {
+        
+        // 一次性添加所有的子控制器
+        addChildViewControllers()
+        
+        // 自定义tabBar
+        let tabBar = TabBar()
+        self.setValue(tabBar, forKeyPath: "tabBar")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    /// 一次性添加所有的子控制器
+    private func addChildViewControllers() {
+        
+        // 分别添加各子控制器
+        addChildViewController(HomeViewController(), title: "首页", imageName: "home")
+        addChildViewController(FishpondViewController(), title: "鱼塘", imageName: "fishpond")
+        // addChildViewController(UIViewController())  // 占位控制器
+        addChildViewController(MessageViewController(), title: "消息", imageName: "message")
+        addChildViewController(AccountViewController(), title: "我的", imageName: "account")
     }
-    */
-
+    
+    /// 添加单个子控制器
+    private func addChildViewController(_ childController: UIViewController, title: String, imageName: String) {
+        
+        // 设置子控制器的标题
+        childController.title = title
+        
+        // 设置子控制器tabBarItem的图片
+        childController.tabBarItem.image = UIImage(named: imageName + "_normal")?.withRenderingMode(.alwaysOriginal)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlight")?.withRenderingMode(.alwaysOriginal)
+        
+        // 设置子控制器tabBarItem字体的颜色
+        var textColor: [String: Any] = Dictionary()
+        textColor[NSForegroundColorAttributeName] = UIColor.black
+        childController.tabBarItem.setTitleTextAttributes(textColor, for: .selected)
+        
+        // 设置子控制器tabBarItem字体的大小
+        var textFont: [String: Any] = Dictionary()
+        textFont[NSFontAttributeName] = UIFont.systemFont(ofSize: 9)
+        childController.tabBarItem.setTitleTextAttributes(textFont, for: .normal)
+        
+        // 将子控制器包装成导航控制器
+        let nav = UINavigationController(rootViewController: childController)
+        
+        // 将导航控制器添加到父控制器中
+        addChildViewController(nav)
+    }
 }
---
+++ XianYuDemo/XianYuDemo/Classes/Main/View/TabBar.swift
@@ -0,0 +1,21 @@
+//
+//  TabBar.swift
+//  XianYuDemo
+//
+//  Created by Enrica on 2017/10/14.
+//  Copyright © 2017年 Enrica. All rights reserved.
+//
+
+import UIKit
+
+class TabBar: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
